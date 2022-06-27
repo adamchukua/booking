6d178d3817f834c1627421633acf6ca0ba7fc72d
thegradle <adamchuk.v.1@gmail.com>
2022-06-27 12:19:30 +0300

    Styles for welcome, login and register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,18 +13,26 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- in sass -->
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2b5797">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="theme-color" content="#2b5797">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="/img/svg/logo.svg" alt="" title="{{ config('app.name', 'Laravel') }}" class="logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -38,17 +46,21 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                        <li class="navbar-item me-3">
+                            <button class="nav-link btn btn-light">Зареєструвати своє помешкання</button>
+                        </li>
+
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <li class="nav-item me-3">
+                                    <a class="nav-link btn" href="{{ route('login') }}">Увійти</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link btn" href="{{ route('register') }}">Зареєструватися</a>
                                 </li>
                             @endif
                         @else
@@ -75,9 +87,20 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="{{ Request::is('/') ? '' : 'py-4' }}">
             @yield('content')
         </main>
+
+        <footer>
+            <div class="footer-top">
+                <button class="footer-top--btn btn btn-light">Зареєструвати своє помешкання</button>
+            </div>
+            <div class="footer-bottom">
+                <div class="footer-bottom--copyright">
+                    Авторські права © 2022 Booking.com. Усі права захищено.
+                </div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
