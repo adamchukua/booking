923a9bd0c9e28c614a0a628252614d4982eff42f
thegradle <adamchuk.v.1@gmail.com>
2022-06-29 11:35:04 +0300

    Delete name validation while register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,8 +19,8 @@
 
                         @error('email')
                         <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
@@ -33,8 +33,8 @@
 
                         @error('password')
                         <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
